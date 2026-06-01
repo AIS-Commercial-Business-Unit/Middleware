1f5917ea21ef5990dd66a721e413ba98c1057bed
apaar.sharma <apaar.sharma@aisembark.onmicorosft.com>
2026-06-01 19:22:01 -0400

    fixes

--- infra/terraform/appgw.tf
+++ infra/terraform/appgw.tf
@@ -38,6 +38,11 @@ resource "azurerm_application_gateway" "main" {
     max_capacity = 2
   }
 
+  ssl_policy {
+    policy_type = "Predefined"
+    policy_name = "AppGwSslPolicy20220101"
+  }
+
   gateway_ip_configuration {
     name      = "gateway-ip-config"
     subnet_id = azurerm_subnet.appgw.id
